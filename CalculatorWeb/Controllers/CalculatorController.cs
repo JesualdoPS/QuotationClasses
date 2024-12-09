6fbafc42615538aceb6bce762227568e0524ed76
Jesualdo Pinheiro Santos Quotation Factory <jesualdo_pinheiro@outlook.com>
2024-12-09 15:12:10 -0300

    Controller Added

---
+++ CalculatorWeb/Controllers/CalculatorController.cs
@@ -0,0 +1,32 @@
+﻿using Calc.BusinessLogic;
+using Calc.Persistance;
+using Microsoft.AspNetCore.Mvc;
+using UnitsNet;
+
+namespace CalculatorWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly Calculator _calculator;
+
+        public CalculatorController(IRepository repository)
+        {
+            _calculator = new Calculator(repository);
+        }
+
+        [HttpPost("Calculate")]
+        public MathLogEntity Calculate(CalculateRequest request)
+        {
+            var mathLog = _calculator.Calculate(request.Input);
+            var mathLogEntity = mathLog.ToEntity();
+            return mathLogEntity;
+        }
+
+        public class CalculateRequest
+        {
+            public string Input { get; set; }
+        }
+    }
+}
---
+++ CalculatorWeb/Program.cs
@@ -0,0 +1,29 @@
+using Calc.BusinessLogic;
+using Calc.Persistance;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddTransient<Calculator>();
+builder.Services.AddTransient<IRepository, RepositorySQL>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+object value = builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
