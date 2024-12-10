a47160f6eb0c52a5527be1a25be5506c359f3a0d
Jesualdo Pinheiro Santos Quotation Factory <jesualdo_pinheiro@outlook.com>
2024-12-10 14:43:31 -0300

    Route name changed

--- CalculatorWeb/Controllers/CalculatorController.cs
+++ CalculatorWeb/Controllers/CalculatorController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace CalculatorWeb.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("Calculator")]
     public class CalculatorController : ControllerBase
     {
         private readonly Calculator _calculator;
