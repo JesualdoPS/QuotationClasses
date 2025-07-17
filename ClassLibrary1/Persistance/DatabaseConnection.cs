f5a14b704786dc44c481c3062eef5653ecd622eb
Jesualdo Pinheiro Santos Quotation Factory <jesualdo.pinheiro.santos@quotationfactory.com>
2025-07-17 16:07:35 -0300

    Database connection organization

--- ClassLibrary1/Persistance/DatabaseConnection.cs
+++ ClassLibrary1/Persistance/DatabaseConnection.cs
@@ -8,8 +8,15 @@ namespace Calc.Persistance
 
         public DatabaseConnection()
         {
-            //connectionString = ConfigurationManager.ConnectionStrings[0].ConnectionString;
-            connectionString = "Data Source=DESKTOP-A7TJSGV;Initial Catalog=dbCalculator;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            connectionString = 
+                "Data Source=DESKTOP-A7TJSGV;" +
+                "Initial Catalog=dbCalculator;" +
+                "Integrated Security=True;" +
+                "Connect Timeout=30;" +
+                "Encrypt=False;" +
+                "Trust Server Certificate=False;" +
+                "Application Intent=ReadWrite;" +
+                "Multi Subnet Failover=False";
         }
 
         public SqlConnection Connect()
